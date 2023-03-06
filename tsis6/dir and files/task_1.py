ae3e6534b4e77cc069a1dfa7346ab33697d810d0
KhojayevaAlsu <122900045+KhojayevaAlsu@users.noreply.github.com>
2023-03-06 20:53:42 +0600

    Delete tsis6 directory

--- tsis6/dir and files/task_1.py
+++
@@ -1,9 +0,0 @@
-'''Write a Python program to list only directories, 
-files and all directories, files in a specified path.'''
-
-import os
-path = input()
-print(os.listdir(path))
-
-
-#C:\Users\khali\Desktop\PP-2\pp2-21B050646\tsis6\dir and files
--- tsis6/dir and files/task_7.py
+++
@@ -1,8 +0,0 @@
-'''Write a Python program to copy the contents of a file to another file'''
-
-import os
-import shutil
-
-file_1 = input()
-file_2 = input()
-shutil.copy(file_1, file_2)
